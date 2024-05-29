c1912a96b2288f9a086af389c3625599ca5d54ea
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-05-29 16:54:59 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-import express from 'express';
 
-const app = express();
+import { setupServer } from './server.js';
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+setupServer();
