8fedf87e835b54aedafcf9b2b45a9f5b1348075e
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-05-30 16:16:09 +0300

    upd

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,6 +2,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+export const PORT = 3000;
+export const MONGODB_USER = 'olgaIK';
+export const MONGODB_PASSWORD = 'Starwars7777';
+export const MONGODB_URL = 'cluster0.llpqstc.mongodb.net';
+export const MONGODB_DB = 'students';
+
 export function env(name, defaultValue) {
   const value = process.env[name];
 
