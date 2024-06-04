cf8690bb1105259b23e071878d7518ffc6770a6d
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-06-04 14:58:35 +0300

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,19 +1,29 @@
-import { HttpError } from 'http-errors';
+import { isHttpError } from 'http-errors';
+import { MongooseError } from 'mongoose';
 
-export const errorHandler = (err, req, res) => {
-    
-  if (err instanceof HttpError) {
-    res.status(err.status).json({
-      status: err.status,
-      message: err.name,
-      data: err,
+export const errorHandler = (error, req, res) => {
+  if (isHttpError(error)) {
+    return res.status(error.status).json({
+      status: error.status,
+      message: error.message,
     });
-    return;
-    }
-    
-    res.status(500).json({
-        status: 500,
-        message: 'Something went wrong',
-        data: err.message,
+  }
+
+  if (error instanceof MongooseError) {
+    return res.status(500).json({
+      status: 500,
+      message: 'Mongoose error',
+      data: {
+        message: error.message,
+      },
+    });
+  }
+
+  res.status(500).json({
+    status: 500,
+    message: 'Internal server error',
+    data: {
+      message: error.message,
+    },
   });
 };
