841a854e0e438c67a4b28d7a9da9ad39d0883064
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-06-04 16:06:26 +0300

    Update contacts.js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,16 +1,33 @@
-import { Router} from 'express';
+import { Router } from 'express';
 import { ctrlWrapper } from '../middlewares/ctrlWrapper.js';
 
-import { getAllcontactsController, getContactByIdController, createContactController, patchContactByIdController } from '../controllers/contacts.js';
+import {
+  getAllcontactsController,
+  getContactByIdController,
+  createContactController,
+  patchContactByIdController,
+  deleteContactController,
+} from '../controllers/contacts.js';
 
 const contactsRouter = Router();
 
 contactsRouter.get('/contacts', ctrlWrapper(getAllcontactsController));
 
-contactsRouter.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+contactsRouter.get(
+  '/contacts/:contactId',
+  ctrlWrapper(getContactByIdController),
+);
 
-contactsRouter.patch('/contacts/:contactId', ctrlWrapper(patchContactByIdController));
+contactsRouter.patch(
+  '/contacts/:contactId',
+  ctrlWrapper(patchContactByIdController),
+);
 
 contactsRouter.post('/contacts', ctrlWrapper(createContactController));
+
+contactsRouter.delete(
+  '/contacts/:contactId',
+  ctrlWrapper(deleteContactController),
+);
 
 export default contactsRouter;
