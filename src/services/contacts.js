9313dbb64b99f1e09b3a47166ce31836463c6782
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-06-10 19:07:25 +0300

    pagination data

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,10 +1,27 @@
 import { ContactsCollection } from '../db/models/contact.js';
 import createHttpError from 'http-errors';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllcontacts = async () => {
-  const contacts = await ContactsCollection.find();
-  return contacts;
-};
+export const getAllcontacts = async ({ page, perPage }) => {
+
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = ContactsCollection.find();
+  const contactsCount = await ContactsCollection.find()
+    .merge(contactsQuery)
+    .countDocuments();
+
+    const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+
+    const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+    return {
+      data: contacts,
+      ...paginationData,
+    };
+  };
+  
 
 export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
