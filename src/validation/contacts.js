c575963323010878e101f283da8f2fcb8c514add
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-06-13 17:05:26 +0300

    Update contacts.js

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,8 +3,8 @@ import Joi from 'joi';
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required().messages({
     'string.base': 'Contact name should be a string',
-    'string.min': 'Contact name should have at least 3 characters',
-    'string.max': 'Contact name should have at most 20 characters',
+    'string.min': 'Contact name should have at least {#limit} characters',
+    'string.max': 'Contact name should have at most {#limit} characters',
     'any.required': 'Contact name is required',
   }),
   phoneNumber: Joi.string().required(),
