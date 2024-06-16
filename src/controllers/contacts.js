a66198cda16cb76f4ef327a0fc67d674bcfc40ad
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-06-16 15:18:45 +0300

    Update contacts.js

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -24,6 +24,7 @@ export const getAllcontactsController = async (req, res) => {
   });
 
   res.status(200).json({
+    status: 200,
     message: 'Successfully found contacts!',
     data: contacts.data,
   });
@@ -45,6 +46,7 @@ export const getContactByIdController = async (req, res, next) => {
   }
 
   res.status(200).json({
+    status: 200,
     message: `Successfully found contact with id ${contactId}`,
     data: contact,
   });
