aab860cee804482930e9b1b1c846309d1488a82c
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-06-20 17:05:53 +0300

    login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,5 @@
 import { registerUser } from '../services/auth.js';
+import { loginUser } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
@@ -9,3 +10,9 @@ export const registerUserController = async (req, res) => {
     data: user,
   });
 };
+
+export const loginUserController = async (req, res) => {
+    await loginUser(req.body);
+  
+    // далі ми доповнемо цей контролер
+  };
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -4,6 +4,9 @@ import { registerUserSchema } from '../validation/auth.js';
 import { registerUserController } from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
 
+import { loginUserSchema} from '../validation/auth.js';
+import { loginUserController } from '../controllers/auth.js';
+
 const router = Router();
 
 router.post(
@@ -12,4 +15,10 @@ router.post(
   ctrlWrapper(registerUserController),
 );
 
+router.post(
+    '/login',
+    validateBody(loginUserSchema),
+    ctrlWrapper(loginUserController),
+  );
+  
 export default router;
