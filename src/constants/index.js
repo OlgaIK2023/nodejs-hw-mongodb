673b00adbeb1ba9d2b0c05aba64f6c0be3ac90a9
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-07-01 15:09:11 +0300

    cloud2

--- src/constants/index.js
+++ src/constants/index.js
@@ -19,6 +19,12 @@ export const SMTP = {
   APP_DOMAIN: 'APP_DOMAIN',
 };
 
+export const CLOUDINARY = {
+  CLOUD_NAME: 'CLOUD_NAME',
+  API_KEY: 'API_KEY',
+  API_SECRET: 'API_SECRET',
+};
+
 
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
